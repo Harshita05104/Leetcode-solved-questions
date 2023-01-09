4bcceb1b478f06447aa793884b813b78b6443f91
Harshita05104 <112561378+Harshita05104@users.noreply.github.com>
2023-01-09 11:19:24 +0530

    Added solution - LeetHub

---
+++ Spirally traversing a matrix - GFG/spirally-traversing-a-matrix.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{   
+    public: 
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
+    {
+        vector<int> ans;
+        int rows=0,rowe=r-1;
+        int cols=0,cole = c-1;
+        while(rows<=rowe && cols<=cole){
+            for(int i=cols;i<=cole;i++){
+                ans.push_back(matrix[rows][i]);
+            }
+            rows++;
+            if(rows>rowe || cols>cole) break;
+            for(int i=rows;i<=rowe;i++){
+                ans.push_back(matrix[i][cole]);
+            }
+            cole--;
+            if(rows>rowe || cols>cole) break;
+            for(int i=cole;i>=cols;i--){
+                ans.push_back(matrix[rowe][i]);
+            }
+            rowe--;
+            if(rows>rowe || cols>cole) break;
+            for(int i= rowe;i>=rows;i--){
+                ans.push_back(matrix[i][cols]);
+            }
+            cols++;
+            if(rows>rowe || cols>cole) break;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int r,c;
+        cin>>r>>c;
+        vector<vector<int> > matrix(r); 
+
+        for(int i=0; i<r; i++)
+        {
+            matrix[i].assign(c, 0);
+            for( int j=0; j<c; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix, r, c);
+        for (int i = 0; i < result.size(); ++i)
+                cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
