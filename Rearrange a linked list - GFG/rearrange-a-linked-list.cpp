5f6b8e74d49042192cdf656a58aa4b2175f733f6
Harshita05104 <112561378+Harshita05104@users.noreply.github.com>
2023-05-27 14:11:08 +0530

    Added solution - LeetHub

---
+++ Rearrange a linked list - GFG/rearrange-a-linked-list.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+// } Driver Code Ends
+/* Node structure
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution
+{
+    public:
+    void rearrangeEvenOdd(Node *head)
+    {
+       if(head->next==NULL) return;
+       
+       Node* temp1 = head;
+       Node* temp2 = head->next;
+       
+       Node* head2 = head->next;
+       
+       while(temp1!=NULL && temp2!=NULL){
+           temp1->next = temp2->next;
+           temp1 = temp1->next;
+           
+           if(temp1==NULL) break;
+           
+           temp2->next = temp1->next;
+           temp2 = temp2->next;
+       }
+       
+       temp1 = head;
+       while(temp1->next!=NULL){
+           temp1 = temp1->next;
+       }
+       
+       temp1->next = head2;
+       
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,i,n,l;
+
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL;
+        struct Node *temp = head;
+        cin>>n;
+        for(i=1; i<=n; i++)
+        {
+            cin>>l;
+
+            if (head == NULL)
+            {   
+                head = temp = new Node(l); 
+                
+            }
+            else
+            {  temp->next = new Node(l);
+               temp = temp->next;
+            }
+        }
+        Solution ob;
+        ob.rearrangeEvenOdd(head);
+        while(head != NULL)
+        {
+            printf("%d ", head->data);
+            head = head->next;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+// } Driver Code Ends
